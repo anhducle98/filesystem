137c663bbbbfab2a19434f31f39854561577e563
Le Minh Quang <quanglm1998@gmail.com>
2018-05-13 10:07:42 +0700

    uncomment callsystem

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,24 @@
 #include "file_system.h"
 #include "interface.h"
 
-// void callsystem() {
-//     file_system_t FS;
-//     FS.create_empty_disk("HD.dat");
+void callsystem() {
+    file_system_t FS;
+    FS.create_empty_disk("HD.dat");
 
-//     FS.create_empty_folder("/", "code");
-//     FS.copy_file_from_outside("/code/", "os.txt");
-//     FS.copy_file_from_outside("/code/", "shit.cpp");
-//     FS.copy_file_from_outside("/", "big.txt");
-//     FS.create_empty_folder("/code/", "cpp");
-//     FS.copy_file_from_outside("/code/cpp/", "shit.cpp");
+    FS.create_empty_folder("/", "code");
+    FS.copy_file_from_outside("/code/", "os.txt");
+    FS.copy_file_from_outside("/code/", "shit.cpp");
+    FS.copy_file_from_outside("/", "big.txt");
+    FS.create_empty_folder("/code/", "cpp");
+    FS.copy_file_from_outside("/code/cpp/", "shit.cpp");
 
-//     FS.read_disk_info();
-//     FS.list_disk();
+    FS.read_disk_info();
+    FS.list_disk();
 
-//     /*FS.delete_folder("/", "code");
-//     FS.read_disk_info();
-//     FS.list_disk();*/
-// }
+    /*FS.delete_folder("/", "code");
+    FS.read_disk_info();
+    FS.list_disk();*/
+}
 
 int main() {
     interface_t shell;
